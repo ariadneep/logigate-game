c4fdf55551e6003dda658f90369d78e727512487
Ariadne Petroulakis <86198636+ariadneep@users.noreply.github.com>
2025-04-08 19:45:31 -0600

    Gate implementation. (#1)
    (1/4) Added some basic classes to gate.h and gate.cpp.
    
    (2/4) Added basic functionality to Gate.
    
    (3/4) Fixed some documentation.
    
    (4/4) Changed Gate methods for convertSignal to return a QString representign the new ID that will be assigned to any wire that is conencted to the output side of this gate. This involved addign wire ID parameters. Updated documentation to reflect this change.

--- gate.cpp
+++ gate.cpp
@@ -1,9 +1,57 @@
 #include "gate.h"
 
-Gate::Gate(QObject *parent)
-    : GridComponent{parent}
-{}
+Gate::Gate(Type type, QObject *parent)
+    : GridComponent{parent}, gateType(type) {
+    outputSignal = false;
+}
 
 Gate::~Gate() {
 
 }
+
+Type Gate::getType() {
+    return gateType;
+}
+
+QString Gate::convertSignal(bool firstSignal, bool secondSignal, QString firstID, QString secondID) {
+    bool newOutput;
+    QString newID = firstID.append(secondID);
+    //calls the specific helper method based on the Type
+    switch(gateType) {
+    case Type::AND:
+        newOutput = firstSignal && secondSignal;
+        break;
+    case Type::OR:
+        newOutput = firstSignal || secondSignal;
+        break;
+    default:
+        return newID;
+    }
+
+    //sets outputSignal to the resulting boolean.
+    outputSignal = newOutput;
+    return newID;
+}
+
+QString Gate::convertSignal(bool input, QString id) {
+    bool newOutput;
+
+    //calls the specific helper method based on the Type
+    switch(gateType) {
+    case Type::NOT:
+        newOutput = !input;
+        break;
+    default:
+        return id;
+    }
+
+    //sets outputSignal to the resulting boolean.
+    outputSignal = newOutput;
+    return id;
+}
+
+bool Gate::getSignal() {
+    return outputSignal;
+}
+
+
--- gate.h
+++ gate.h
@@ -23,20 +23,64 @@ class Gate : public GridComponent {
 public:
     /**
      * @brief Gate - Makes a Gate object.
+     * @param type - AND, OR, or NOT gate.
      * @param parent - the parent object
      */
-    explicit Gate(QObject *parent = nullptr);
+    explicit Gate(Type type, QObject *parent = nullptr);
 
     /**
      * Destructor for the Gate class.
      */
     ~Gate();
 
+    /**
+     * @brief getType - returns either Type::AND if this gate
+     * represents an AND gate, Type::OR if this gate represents
+     * an OR gate, or Type::NOT if this gate represents a NOT gate.
+     * @return a gate Type
+     */
+    Type getType();
+
+    /**
+     * @brief getSignal - returns the output signal
+     * @return the current signal the inputs merge into.
+     */
+    bool getSignal();
+
+    /**
+     * @brief convertSignal - sets the output signal after checking the
+     * two input pulses and performing a boolean operation on them based
+     * on the Type of the gate.
+     * @param firstSignal - the signal of the wire in the gate's first slot
+     * @param secondSignal - the signal of the wire in the gate's second input slot.
+     * @param firstID - the ID linked with the first signal.
+     * @param secondID - the ID linked with the second signal.
+     * @return - the new, concatenated ID of the resulting signal conversion/transformation.
+     */
+    QString convertSignal(bool firstSignal, bool secondSignal, QString firstID, QString secondID);
+
+    /**
+     * @brief convertSignal - sets the output signal to an opertion
+     * based on a compatible gate Type when given only one input. Keeps
+     * track of the ID of the input passed into it.
+     * @param input - the boolean signal sent in by the input wire.
+     * @param id - the ID associated with the input signal.
+     * @return the unchanged ID of the resulting signal conversion/transformation.
+     */
+    QString convertSignal(bool input, QString id);
+
 private:
     /**
-     * @brief gateType - if it's an and, or, or not gate.
+     * @brief gateType - whether this object represents
+     * an AND, OR, or NOT gate.
      */
     Type gateType;
+
+    /**
+     * @brief outputSignal - the signal currently output
+     * by this logic gate. False when the gate is dead,
+     */
+    bool outputSignal;
 
 };
 
